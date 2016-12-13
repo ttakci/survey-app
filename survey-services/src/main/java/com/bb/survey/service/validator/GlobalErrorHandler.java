47662f9682452229ecc1483af4df59d436054e21
ttakci <tolgatakci@gmail.com>
2016-12-14 01:52:17 +0200

    project config

--- survey-services/src/main/java/com/bb/survey/service/validator/GlobalErrorHandler.java
+++ survey-services/src/main/java/com/bb/survey/service/validator/GlobalErrorHandler.java
@@ -2,6 +2,7 @@ package com.bb.survey.service.validator;
 
 import java.util.List;
 import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -9,11 +10,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+
+
+@ControllerAdvice
 public class GlobalErrorHandler {
 
 	@Autowired
